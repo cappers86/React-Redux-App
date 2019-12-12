0e937205c0581cb479d36cbb5c0baab6c2d524f9
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-12-12 16:04:52 +0000

    Made some small changes

--- react-app/src/state/actionTypes.js
+++ react-app/src/state/actionTypes.js
@@ -2,12 +2,17 @@ import axios from "axios";
 import actionCreators, {FETCH_DATA_START,FETCH_DATA_SUCCESS,FETCH_DATA_FAIL} from './actionCreators';
 
 
-export const getUser = () => dispatch => {
+export const getUser = () => (dispatch) => {
     dispatch({ type: FETCH_DATA_START });
     
     axios   
       .get(`https://api.github.com/users/cappers86`)
-      .then(res => dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data }))
+      
+      .then((res)=>{
+        console.log(res.data)
+       dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data })
+      })
+      
       .catch(err => dispatch({ type: FETCH_DATA_FAIL, payload: err }));
       
   };
